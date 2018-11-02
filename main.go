50f14ef838dee5d6175fd22112f18b178a07b53c
Osric Wilkinson <osric@fluffypeople.com>
2018-11-02 21:26:31 +0000

    Now moves round the map!

--- main.go
+++ main.go
@@ -1,13 +1,81 @@
 package main
 
-import "github.com/nsf/termbox-go"
-import "log"
+import (
+	"github.com/nsf/termbox-go"
+	"log"
+)
+
+type logger struct {
+	lines   []string
+	display int
+}
+
+func (l *logger) log(msg string) {
+	l.lines = append(l.lines, msg)
+}
+
+func (l *logger) upTo() int {
+	if l.display < len(l.lines) {
+		return l.display
+	}
+	return len(l.lines)
+}
+
+func (l *logger) draw(x, y int) {
+	for i := 0; i < l.upTo(); i++ {
+		drawString(x, y+i, l.lines[len(l.lines)-(i+1)])
+	}
+}
+
+type sprite struct {
+	x, y int
+	c    rune
+}
+
+func (s *sprite) move(dx, dy int) {
+	width, height := termbox.Size()
+	s.x += dx
+	if s.x < 0 {
+		s.x = 0
+	} else if s.x > width {
+		s.x = width
+	}
+	s.y += dy
+	if s.y < 0 {
+		s.y = 0
+	} else if s.y > height {
+		s.y = height
+	}
+}
+
+func (s *sprite) handleKeyEvent(e termbox.Event) {
+	if e.Ch != 0 {
+		return
+	}
+	switch e.Key {
+	case termbox.KeyArrowLeft:
+		s.move(-1, 0)
+		return
+	case termbox.KeyArrowRight:
+		s.move(1, 0)
+		return
+	case termbox.KeyArrowUp:
+		s.move(0, -1)
+		return
+	case termbox.KeyArrowDown:
+		s.move(0, 1)
+		return
+	}
+}
+
+func (s *sprite) draw() {
+	drawString(s.x, s.y, string(s.c))
+}
 
 func drawString(x, y int, msg string) {
 	for i, c := range msg {
 		termbox.SetCell(x+i, y, c, termbox.ColorBlue, termbox.ColorBlack)
 	}
-	termbox.Flush()
 }
 
 func drawBox(top, left, bottom, right int, c rune) {
@@ -22,6 +90,12 @@ func drawBox(top, left, bottom, right int, c rune) {
 	}
 
 	termbox.Flush()
+}
+
+func drawMap() {
+	drawString(5, 5, "Hello")
+	drawString(12, 5, "World")
+	drawBox(4, 4, 6, 18, '#')
 }
 
 func main() {
@@ -39,9 +113,22 @@ func main() {
 		}
 	}()
 
-	drawString(5, 5, "Hello")
-	drawString(12, 5, "World")
-
-	drawBox(4, 4, 6, 18, '#')
-	<-events
+	var player = sprite{
+		x: 9,
+		y: 9,
+		c: '@',
+	}
+	//var l = logger{display: 3}
+	for e := range events {
+		if e.Ch == 'q' {
+			break
+		}
+		termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+		drawMap()
+		if e.Type == termbox.EventKey {
+			player.handleKeyEvent(e)
+			player.draw()
+		}
+		termbox.Flush()
+	}
 }
